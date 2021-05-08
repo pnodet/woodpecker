edfb32a0dbb1832ea33ecb9b9ab2cd959730d01c
Paul Nodet <paul.nodet@gmail.com>
2021-05-08 18:05:38 +0200

    FIX: SRC/Scripts

--- src/main.js
+++ src/main.js
@@ -9,4 +9,23 @@ console.log(await func.getUserData('vilouza'));
 
 let chess = await getOpening('White');
 
-func.getGames('detnop')
+func.getGames('detnop');
+
+generatePositions = pgn => {
+  let games = pgn.split('\n\n');
+  let positions = [];
+  for (let game of games) {
+    let tempChess = new Chess();
+    if (tempChess.load_pgn(game)) {
+      let history = tempChess.history();
+      if (history) {
+        let chess = new Chess();
+        for (let move of history) {
+          chess.move(move);
+          let fen = chess.fen().split(' ')[0] + ' w KQkq - 0 1';
+          positions.push(fen);
+        }
+      }
+    }
+  }
+};
--- src/openingParser.js
+++ src/openingParser.js
@@ -1,8 +1,8 @@
-import {promises as fs} from 'fs';
+import {readFile} from 'fs/promises';
 import {Chess} from '../modules/cm-chess/Chess.mjs';
 
 async function getOpening(color) {
-  const pgn = await fs.readFile(`./games/${color}.pgn`, 'utf8');
+  const pgn = await readFile(`./games/${color}.pgn`, 'utf8');
   const chess = new Chess();
   chess.loadPgn(pgn);
   return chess;
